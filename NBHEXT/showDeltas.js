aca6f16f7685e07940e1c8ea461b81f7ede1d0f9
Maxim Molchanov <max@looksery.com>
2015-12-16 02:16:10 +0200

    Server changed to nbhext.com. [ISSUE-2] Fixed zero rating delta for legendary grandmasters. Removed background.

--- NBHEXT/showDeltas.js
+++ NBHEXT/showDeltas.js
@@ -11,10 +11,7 @@
 */
 
 var partyNum = 0;
-var ratings = [];
-var places = [];
 var deltas = [];
-var numbers = [];
 
 function modifyPartyHtml(index, elem)
 {
@@ -22,8 +19,12 @@ function modifyPartyHtml(index, elem)
     if (partyNum > 0)
     {
         var handle = $(elem).find("td:eq(1)").find("a").first().html();
-        if (handle in deltas)
-            delta = Math.round(deltas[handle]);
+        if (typeof handle != 'undefined')
+        {
+            handle = handle.replace(/<\/?[^>]+>/gi, '');
+            if (handle in deltas)
+                delta = deltas[handle];
+        }
     }
     var text;
     if (partyNum == 0)
@@ -50,24 +51,12 @@ function showDeltas()
     if (count > 2)
     {
         var contestId = document.location.href.replace(/\D+/ig, ',').substr(1).split(',')[0];
-        $.getJSON("http://codeforces.com/api/contest.standings?contestId=" + contestId,
+        $.getJSON("http://nbhext.com/api/standings?contestId=" + contestId,
             function(data)
             {
-                var handles = [];
-                for (var i = 0; i < data.result.rows.length; ++i)
+                if (data.result == "OK")
                 {
-                    places[i] = data.result.rows[i].rank;
-                    handles.push(data.result.rows[i].party.members[0].handle);
-                }
-                chrome.extension.sendRequest({"handles" : handles}, function(storedRatings) {
-                    if (storedRatings.length == handles.length)
-                    {
-                        for (var i = 0; i < storedRatings.length; ++i)
-                        {
-                            ratings[i] = (storedRatings[i] != 0 ? storedRatings[i] : 1500);
-                        }
-                        deltas = CalculateRatingChanges(ratings, places, handles);
-                    }
+                    deltas = data.deltas;
                     $(".standings").find("tr").first().find("th").last().removeClass("right");
                     $(".standings").find("tr").find("td").removeClass("right");
                     $(".standings").find("tr").each(modifyPartyHtml);
@@ -75,7 +64,7 @@ function showDeltas()
                         $(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom dark right'> </td>");
                     else
                         $(".standings").find("tr").last().find("td").last().replaceWith("<td class='smaller bottom right'> </td>");
-                });
+                }
             }
         );
     }
